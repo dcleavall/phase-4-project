6e7e3f7c56247dd31f7803163269f9cbf95fe20d
Derrick <dcleavall@gmail.com>
2023-07-17 16:04:27 -0600

    updated like and comment box for users to interact in the thread

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,9 +3,15 @@ import { useDrop } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 import { AuthContext } from './UserContext';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { EmojiHappyIcon } from "@heroicons/react/outline";
+
 
 const Dashboard = () => {
-  const [droppedMindfulness, setDroppedMindfulness] = useState(null);
+  const [droppedMindfulness, setDroppedMindfulness] = useState({});
+  const [commentText, setCommentText] = useState('');
+  const [liked, setLiked] = useState(false);
   const { user } = useContext(AuthContext);
   const history = useHistory();
 
@@ -49,29 +55,37 @@ const Dashboard = () => {
 
     // Update the state with the dropped mindfulness
     setDroppedMindfulness(mindfulness);
+    // Save the dropped mindfulness to localStorage
+    saveDroppedMindfulnessToLocalStorage(mindfulness);
   };
 
-  // ... (existing code)
+  // Helper function to save dropped mindfulness to localStorage
+  const saveDroppedMindfulnessToLocalStorage = (mindfulnessData) => {
+    try {
+      localStorage.setItem('droppedMindfulness', JSON.stringify(mindfulnessData));
+    } catch (error) {
+      console.error('Failed to save dropped mindfulness to localStorage:', error);
+    }
+  };
 
+  // Helper function to load dropped mindfulness from localStorage
+  const loadDroppedMindfulnessFromLocalStorage = () => {
+    try {
+      const mindfulnessData = localStorage.getItem('droppedMindfulness');
+      return mindfulnessData ? JSON.parse(mindfulnessData) : {};
+    } catch (error) {
+      console.error('Failed to load dropped mindfulness from localStorage:', error);
+      return {};
+    }
+  };
+
+  // Load the dropped mindfulness from localStorage when the component mounts
   useEffect(() => {
     if (!user) {
       // Fetch user data or handle the loading state here
     } else {
-      // Fetch the dropped mindfulness data from the backend using fetch
-      fetch('/mindfulness')
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Failed to fetch dropped mindfulness data');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          // Update the state with the dropped mindfulness data
-          setDroppedMindfulness(data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      const persistedDroppedMindfulness = loadDroppedMindfulnessFromLocalStorage();
+      setDroppedMindfulness(persistedDroppedMindfulness);
     }
   }, [user]);
 
@@ -93,6 +107,19 @@ const Dashboard = () => {
     textAlign: 'center',
   };
 
+  const handleComment = () => {
+    // You can implement the logic for posting a comment here
+    console.log('Posted a comment:', commentText);
+    // Clear the comment text box after posting the comment
+    setCommentText('');
+  };
+
+  const handleLike = () => {
+    // You can implement the logic for liking the post here
+    setLiked(!liked);
+    console.log('Liked the post!');
+  };
+
   return (
     <div className="bg-gray-500 flex flex-col items-center min-h-screen">
       {!user ? (
@@ -100,7 +127,7 @@ const Dashboard = () => {
       ) : (
         <>
           <h1 className="text-3xl font-bold mb-4 text-center">Community Thread</h1>
-          <div ref={drop} style={dropzoneStyle}>
+          <div ref={drop} className="container mt-5 mb-5" style={{ ...dropzoneStyle }}>
             <p>Drop mindfulness here</p>
           </div>
           {droppedMindfulness && (
@@ -110,6 +137,44 @@ const Dashboard = () => {
               <p>Type: {droppedMindfulness.type}</p>
               <p>Duration: {droppedMindfulness.duration}</p>
               <p>Notes: {droppedMindfulness.notes}</p>
+
+              {/* Like Button */}
+              <div className="px-4 flex justify-between">
+                <button
+                  onClick={handleLike}
+                  className={`h-4 ${liked ? 'text-red-500' : 'text-gray-500'}`}
+                >
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                </button>
+              </div>
+              <div className="px-4 truncate flex justify-between">
+                <button
+                  onClick={handleLike}
+                  className={`h-4 ${liked ? 'text-red-500' : 'text-gray-500'}`}
+                >
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                </button>
+              </div>
+              <hr />
+
+              {/* Comment Textbox */}
+              <form className="flex items-center p-4">
+                <EmojiHappyIcon className="h-7 mr-2" />
+                <input
+                  type="text"
+                  className="border-none flex-1 focus:ring-0 outline-none"
+                  placeholder="Add a comment..."
+                  value={commentText}
+                  onChange={(e) => setCommentText(e.target.value)}
+                />
+                <button
+                  type="submit"
+                  onClick={handleComment}
+                  className="font-semibold text-blue-400"
+                >
+                  Post
+                </button>
+              </form>
             </div>
           )}
         </>
